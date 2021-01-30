75d3332e1e94994cd966594669ed51966e18f9a7
FinixYang <jz_yangf@163.com>
2021-01-30 20:24:59 +0800

    modify render list num

--- src/components/styled/slidingList.js
+++ src/components/styled/slidingList.js
@@ -1,10 +1,7 @@
 import styled from "styled-components";
 
 export const SlidingListWarper = styled.div`
-    width: ${(props)=>{
-        console.log(props);
-        return `${props.width || 500}px`;
-    } };
+    width: ${(props)=> `${props.width || 500}px` };
     height: ${(props)=> `${props.height * props.listNum + 40 || 280}px`};
     border: 1px #CCC solid;
     overflow: hidden;
